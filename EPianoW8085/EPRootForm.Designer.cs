649ffbdf3d4f53cecaa2800a34bfb4f403fecede
itKhaiNDdotDev <khai12345678t@gmail.com>
2022-01-15 00:00:40 +0700

    Commit code ver0_Demo

---
+++ EPianoW8085/EPRootForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace EPianoW8085
+{
+    partial class EPRootForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hdApp = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(2, 72);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1284, 240);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(557, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(121, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(210)))), ((int)(((byte)(230)))));
+            this.label3.Location = new System.Drawing.Point(377, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(484, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(210)))), ((int)(((byte)(230)))));
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // hdApp
+            // 
+            this.hdApp.AutoSize = true;
+            this.hdApp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hdApp.Font = new System.Drawing.Font("Lucida Sans Unicode", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.hdApp.ForeColor = System.Drawing.Color.DarkGreen;
+            this.hdApp.Location = new System.Drawing.Point(40, 9);
+            this.hdApp.Name = "hdApp";
+            this.hdApp.Size = new System.Drawing.Size(1222, 55);
+            this.hdApp.TabIndex = 1;
+            this.hdApp.Text = " ===   Electronic Piano with Mcr8051 firmware   === ";
+            this.hdApp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EPRootForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(1284, 300);
+            this.Controls.Add(this.hdApp);
+            this.Controls.Add(this.panel1);
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Name = "EPRootForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Electronic Piano with 8051";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label hdApp;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EPianoW8085/EPRootForm.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace EPianoW8085
+{
+    public partial class EPRootForm : Form
+    {
+        static SerialPort _sPort;
+
+        public EPRootForm()
+        {
+            InitializeComponent();
+
+            _sPort = new SerialPort();
+            _sPort.PortName = "COM5";
+            _sPort.BaudRate = 9600;
+            _sPort.Open();
+        }
+
+        //public void configPort()
+        //{
+        //    _serialPort = new SerialPort();
+        //    _serialPort.PortName = "COM5";
+        //    _serialPort.BaudRate = 9600;
+        //    //_serialPort.Parity = //Parity;
+        //    //_serialPort.DataBits = INT;
+        //    //_serialPort.StopBits = //StopBits;
+        //    //_serialPort.Handshake = //Handshake;
+
+        //    string message;
+        //    //StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+        //    //Thread readThread = new Thread(Read);
+        //    //// Set the read/write timeouts
+        //    //_serialPort.ReadTimeout = 500;
+        //    //_serialPort.WriteTimeout = 500;
+        //    _serialPort.Open();
+        //    //readThread.Start();
+        //    while (true)
+        //    {
+        //        message = "Demo Demoday";
+        //        _serialPort.WriteLine(String.Format("<data>: {0}", message));
+        //        string messageR = _serialPort.ReadLine();
+        //        MessageBox.Show(messageR);
+        //    }
+        //    //readThread.Join();
+        //    //Read();
+            
+        //    //_serialPort.Close();
+        //}
+
+        //public static void Read()
+        //{
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            string message = _serialPort.ReadLine();
+        //            MessageBox.Show(message);
+        //        }
+        //        catch (TimeoutException) { }
+        //    }
+        //}
+
+        List<Label> listLbls = new List<Label>();
+        List<Label> listLblBNs = new List<Label>();
+
+        // Danh sach dinh nghia ky tu tuong ung voi index cua phim dan
+        Dictionary<Keys, int> keyToId = new Dictionary<Keys, int>()
+        {
+            {Keys.Z, 0},
+            {Keys.C, 1},
+            {Keys.B, 2},
+            {Keys.N, 3},
+            {Keys.A, 4},
+            {Keys.D, 5},
+            {Keys.G, 6},
+            {Keys.H, 7},
+            {Keys.K, 8},
+            {Keys.Q, 9},
+            {Keys.W, 10},
+            {Keys.R, 11},
+            {Keys.Y, 12},
+            {Keys.I, 13},
+            {Keys.O, 14},
+            {Keys.D1, 15},
+            {Keys.D3, 16},
+            {Keys.D4, 17},
+            {Keys.D6, 18},
+            {Keys.D8, 19},
+            {Keys.D0, 20}
+        };
+        Dictionary<Keys, int> keyToIdBN = new Dictionary<Keys, int>()
+        {
+            {Keys.X, 0},
+            {Keys.V, 1},
+            {Keys.M, 2},
+            {Keys.S, 3},
+            {Keys.F, 4},
+            {Keys.J, 5},
+            {Keys.L, 6},
+            {Keys.E, 7},
+            {Keys.T, 8},
+            {Keys.U, 9},
+            {Keys.P, 10},
+            {Keys.D2, 11},
+            {Keys.D5, 12},
+            {Keys.D7, 13},
+            {Keys.D9, 14}
+        };
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //List<Panel> listPns = new List<Panel>();
+            //List<Panel> listPnBNs = new List<Panel>();
+            int n = 21; // Kich thuoc Piano tinh theo so phim dan trang
+            int Ww = 60, Hw = 160;  // Kich thuoc phim dan trang
+            int Wb = Ww / 3 * 2, Hb = Hw / 3 * 2;   // Kich thuoc phim dan den
+            int Yp = 0;    // Diem dat phim dan
+            // Yp chua co tac dung ==========================================
+
+            for (int i = 0; i < n; i++)
+            {
+                Label p = new Label();
+
+                //Button b = new Button();
+                //b.Text = "Demo Click";
+                //b.AutoSize = true;
+                //p.Controls.Add(b);
+
+                if (((i % 7) == 0 || (i % 7) == 1 || (i % 7) == 3 || (i % 7) == 4 || (i % 7) == 5) && (i != n - 1))
+                {
+                    Label pt = new Label();
+                    pt.BackColor = Color.Black;
+                    pt.Width = Wb;
+                    pt.Height = Hb;
+                    if(i == 0)
+                        pt.Location = new Point(panel1.Location.X + 3 + Ww - Wb/2, panel1.Location.Y + Yp + 1);
+                    else
+                        pt.Location = new Point(listLbls[i - 1].Location.X + 2*Ww + 3 - Wb / 2, listLbls[i - 1].Location.Y + 1);
+                    panel1.Controls.Add(pt);
+                    listLblBNs.Add(pt);
+                }
+
+                listLbls.Add(p);
+                listLbls[i].BackColor = Color.White;
+                listLbls[i].Width = Ww;
+                listLbls[i].Height = Hw;
+                listLbls[i].Text = keyToId.Keys.ElementAt(i).ToString();
+                listLbls[i].TextAlign = ContentAlignment.BottomCenter;
+                listLbls[i].ForeColor = Color.DarkBlue; keyToId.Keys.ElementAt(i).ToString();
+                //if (i == 4)
+                //    listLbls[i].Text = ",";
+                //if (i == 5)
+                //    listLbls[i].Text = "/";
+                //if (i == 11)
+                //    listLbls[i].Text = ";";
+                //if (i == 19)
+                //    listLbls[i].Text = "]";
+                //if (i == 20)
+                //    listLbls[i].Text = "`~";
+                //if (i == 27)
+                //    listLbls[i].Text = "=";
+                listLbls[i].TextAlign = ContentAlignment.BottomCenter;
+                listLbls[i].ForeColor = Color.DarkBlue;
+                //listLbls[i].  CHINH FONT CHU;
+                if (i == 0)
+                    listLbls[i].Location = new Point(panel1.Location.X + 2, panel1.Location.Y + Yp);
+                else
+                    listLbls[i].Location = new Point(listLbls[i - 1].Location.X + Ww + 1, listLbls[i - 1].Location.Y);
+
+                panel1.Controls.Add(listLbls[i]);
+            }
+            for (int i = 0; i < n/7*5; i++)
+            {
+                listLblBNs[i].Text = keyToIdBN.Keys.ElementAt(i).ToString();
+                //if (i == 3)
+                //    listLblBNs[i].Text = ".";
+                //if (i == 8)
+                //    listLblBNs[i].Text = "''";
+                //if (i == 13)
+                //    listLblBNs[i].Text = "[";
+                //if (i == 14)
+                //    listLblBNs[i].Text = "\\";
+                //if (i == 19)
+                //    listLblBNs[i].Text = "-";
+                listLblBNs[i].TextAlign = ContentAlignment.BottomCenter;
+                listLblBNs[i].ForeColor = Color.Yellow;
+            }
+            panel1.AutoSize = true;
+
+            // Them cac label de de chu thich cho ban phim
+            //for (int i = 0; i < 28; i++)
+            //{ 
+            //    Label lbl = new Label();
+            //    lbl.Text = keyToId.Keys.ElementAt(i).ToString();
+            //    lbl.BorderStyle = BorderStyle.None;
+            //    lbl.TextAlign = ContentAlignment.MiddleCenter;
+            //    lbl.ForeColor = Color.DarkBlue;
+            //    lbl.Location = new Point(50, 150);
+            //    //if (i == 0)
+            //    //    lbl.Location = new Point(panel1.Location.X + 4, panel1.Location.Y + Yp + Hw*3/4);
+            //    //else
+            //    //    lbl.Location = new Point(listPns[i - 1].Location.X + Ww + 3, listPns[i - 1].Location.Y + Hw*3/4);
+            //    listPns[i].Controls.Add(lbl);
+            //}
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    Label lbl = new Label();
+            //    lbl.Text = keyToIdBN.Keys.ElementAt(i).ToString();
+            //    lbl.BorderStyle = BorderStyle.None;
+            //    lbl.TextAlign = ContentAlignment.MiddleCenter;
+            //    lbl.ForeColor = Color.Yellow;
+            //    listPnBNs[i].Controls.Add(lbl);
+            //}
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string sData;
+            for(int i = 0; i < 21; i++)
+            {
+                if (e.KeyCode == keyToId.Keys.ElementAt(i))
+                {
+                    listLbls[i].BackColor = Color.LightSalmon;
+                    if (i == 15)
+                        sData = "1";
+                    else if(i == 16)
+                        sData = "3";
+                    else if (i == 17)
+                        sData = "4";
+                    else if (i == 18)
+                        sData = "6";
+                    else if (i == 19)
+                        sData = "8";
+                    else if (i == 20)
+                        sData = "0";
+                    else
+                        sData = keyToId.Keys.ElementAt(i).ToString();
+                    _sPort.Write(sData);
+                }
+            }
+            //MessageBox.Show(Text = e.KeyCode.ToString());
+            for (int i = 0; i < 15; i++)
+            {
+                if (e.KeyCode == keyToIdBN.Keys.ElementAt(i))
+                {
+                    listLblBNs[i].BackColor = Color.LightSlateGray;
+                    if(i == 11)
+                        sData = "1";
+                    else if (i == 12)
+                        sData = "6";/////////CAN CHINH SUA
+                    else if (i == 13)
+                        sData = "8";
+                    else if (i == 14)
+                        sData = "0";
+                    else
+                        sData = keyToIdBN.Keys.ElementAt(i).ToString();
+                    _sPort.Write(sData);
+                }
+            }
+
+            //configPort();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            for (int i = 0; i < 21; i++)
+            {
+                if (e.KeyCode == keyToId.Keys.ElementAt(i))
+                {
+                    listLbls[i].BackColor = Color.White;
+                }
+            }
+            for (int i = 0; i < 15; i++)
+            {
+                if (e.KeyCode == keyToIdBN.Keys.ElementAt(i))
+                {
+                    listLblBNs[i].BackColor = Color.Black;
+                }
+            }
+        }
+    }
+}
